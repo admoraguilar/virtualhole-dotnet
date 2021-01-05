b81aae1473867a9eb33d9848103b1c6dd29b7c9e
Admor Aguilar <admor.aguilar@gmail.com>
2021-01-05 22:35:40 +0800

    * Create Sandbox project for experimenting on architectures

---
+++ VirtualHole.Sandbox/Program.cs
@@ -0,0 +1,18 @@
+﻿using Midnight.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualHole.Sandbox
+{
+	class Program
+	{
+		private static void Main(string[] args)
+		{
+			MLog.Log(MLogLevel.Warning, "Hello World.");
+			Console.ReadLine();
+		}
+	}
+}
