0f528d4a057278b0bed058fcc6c450ce620ec239
Admor Aguilar <admor.aguilar@gmail.com>
2021-01-08 12:31:30 +0800

    * Log written and deleted content on the DB

--- VirtualHole.Scraper/Scraper/Content/Steps/ContentWriteToDBStep.cs
+++ VirtualHole.Scraper/Scraper/Content/Steps/ContentWriteToDBStep.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Midnight.Logs;
 using Midnight.Tasks;
 using Midnight.Pipeline;
+using VirtualHole.DB.Contents;
+using System.Collections.Generic;
 
 namespace VirtualHole.Scraper
 {
@@ -29,10 +31,19 @@ namespace VirtualHole.Scraper
 				MLog.Log(nameof(ContentWriteToDBStep), $"Will deleting non-existent content: {Context.OutDeletedResults.Count}...");
 
 				MLog.Log(nameof(ContentWriteToDBStep), "Writing...");
+				LogContentsTitle(Context.OutNewResults);
 				await Context.InDB.Contents.UpsertManyContentsAsync(Context.OutNewResults);
 
 				MLog.Log(nameof(ContentWriteToDBStep), "Deleting...");
+				LogContentsTitle(Context.OutDeletedResults);
 				await Context.InDB.Contents.DeleteManyAsync(Context.OutDeletedResults);
+
+				void LogContentsTitle(IEnumerable<Content> contents)
+				{
+					foreach(Content content in contents) {
+						MLog.Log(nameof(ContentWriteToDBStep), $"Social: {content.SocialType} | Type: {content.ContentType} | Title: {content.Title}");
+					}
+				}
 			}
 		}
 	}
